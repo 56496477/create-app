91c149ed2379cf0442c373c563a7941d4259c0f8
niufang <56496477@qq.com>
2020-01-06 18:42:30 +0800

    add..

--- bin/index.js
+++ bin/index.js
@@ -13,13 +13,32 @@ const spinner = new Ora();
 program.version("0.0.1", "-v, --version", "new version message");
 
 program
-
   .command("create")
-
   .description("Create a new react template")
-
   .action(() => {
-    
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select the scaffolding type:',
+            name: 'type',
+            choices: [
+                "javascript",
+                "typescript",
+            ]
+        }
+    ]).then(({ type }) => {
+        if(type === 'javascript') {
+            downloadJs();
+        }
+        if(type === 'typescript') {
+            downloadTs();
+        }
+    })
+
+});
+
+function downloadJs() {
     inquirer.prompt([
         {
             type: 'input',
@@ -33,11 +52,6 @@ program
             console.log(chalk.bold.red('The directory already exists, please input!'));
             process.exit(0);
         }
-
-        // if (fs.existsSync(path.resolve('template-react'))) {
-        //     console.log(chalk.bold.red('The template-react default directory already exists, please remove the retry!'));
-        //     process.exit(0);
-        // }
 
         spinner.start("generated...");
 
@@ -63,9 +77,50 @@ program
             console.log(chalk.blue(`enjoy it !`));
 
         });
-
     })
+}
 
-});
+function downloadTs() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'targetDir',
+            message: chalk.blue(`Build directory(${chalk.bold.red(` default:./template-react-ts `)}):`),
+            default: () => 'template-react-ts'
+        }
+    ]).then(({ targetDir }) => {
+        
+        if (fs.existsSync(path.resolve(targetDir))) {
+            console.log(chalk.bold.red('The directory already exists, please input!'));
+            process.exit(0);
+        }
+
+        spinner.start("generated...");
+
+        exec('git clone https://github.com/56496477/typescript-cli.git', (error, stdout, stderr) => {
+
+            if (error) {
+                console.log(chalk.bold.red('Pull error !'));
+                process.exit(0);
+                return;
+            }
+
+            if (targetDir !== 'template-react-ts') {
+                exec(`mv template-react/ ${targetDir}`);
+            }
+
+            exec(`rm -rf ${targetDir}/.git`);
+
+            spinner.succeed('Successful...!');
+
+            console.log(chalk.green`The project has been built successfully: `)
+            console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
+            console.log(chalk.blue(`2.yarn or npm install`));
+            console.log(chalk.blue(`enjoy it !`));
+
+        });
+    })
+}
+
 
 program.parse(process.argv);
