c2cef1d13cc737ad3124a6f489d6e258b7fd7000
niufang <56496477@qq.com>
2020-01-09 11:27:32 +0800

    add..

--- bin/index.js
+++ bin/index.js
@@ -15,9 +15,9 @@ program.version("0.0.1", "-v, --version", "new version message");
 program
   .command("create")
   .description("Create a new react template")
-  .action(() => {
+  .action(async () => {
 
-    inquirer.prompt([
+    const { type } = await inquirer.prompt([
         {
             type: 'list',
             message: 'Please select the scaffolding type:',
@@ -27,99 +27,101 @@ program
                 "typescript",
             ]
         }
-    ]).then(({ type }) => {
-        if(type === 'javascript') {
-            downloadJs();
-        }
-        if(type === 'typescript') {
-            downloadTs();
-        }
-    })
+    ])
+
+    if(type === 'javascript') {
+        await downloadJs();
+    }
+    if(type === 'typescript') {
+        await downloadTs();
+    }
 
 });
 
-function downloadJs() {
-    inquirer.prompt([
+async function downloadJs() {
+
+    const { targetDir } = await inquirer.prompt([
         {
             type: 'input',
             name: 'targetDir',
-            message: chalk.blue(`Build directory(${chalk.bold.red(` default:./template-react `)}):`),
-            default: () => 'template-react'
+            message: chalk.blue(`${chalk.bold.red(`Build directory`)}:`),
+            validate: function(val = '') {
+                return val !== '';
+            }
         }
-    ]).then(({ targetDir }) => {
-        
-        if (fs.existsSync(path.resolve(targetDir))) {
-            console.log(chalk.bold.red('The directory already exists, please input!'));
+    ]);
+
+    if (fs.existsSync(path.resolve(targetDir))) {
+        console.log(chalk.bold.red('The directory already exists, please input!'));
+        process.exit(0);
+    }
+
+    spinner.start("generated...");
+
+    exec('git clone https://github.com/56496477/template-react.git', (error) => {
+
+        if (error) {
+            console.log(chalk.bold.red('Pull error !'));
             process.exit(0);
         }
 
-        spinner.start("generated...");
+        if (targetDir !== 'template-react') {
+            exec(`mv template-react/ ${targetDir}`);
+        }
 
-        exec('git clone https://github.com/56496477/template-react.git', (error, stdout, stderr) => {
+        exec(`rm -rf ${targetDir}/.git`);
 
-            if (error) {
-                console.log(chalk.bold.red('Pull error !'));
-                process.exit(0);
-                return;
-            }
+        spinner.succeed('Successful...!');
 
-            if (targetDir !== 'template-react') {
-                exec(`mv template-react/ ${targetDir}`);
-            }
+        console.log(chalk.green`The project has been built successfully: `)
+        console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
+        console.log(chalk.blue(`2.yarn or npm install`));
+        console.log(chalk.blue(`enjoy it !`));
 
-            exec(`rm -rf ${targetDir}/.git`);
-
-            spinner.succeed('Successful...!');
-
-            console.log(chalk.green`The project has been built successfully: `)
-            console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
-            console.log(chalk.blue(`2.yarn or npm install`));
-            console.log(chalk.blue(`enjoy it !`));
-
-        });
-    })
+    });
 }
 
-function downloadTs() {
-    inquirer.prompt([
+async function downloadTs() {
+
+    const { targetDir } = await inquirer.prompt([
         {
             type: 'input',
             name: 'targetDir',
-            message: chalk.blue(`Build directory(${chalk.bold.red(` default:./template-react-ts `)}):`),
-            default: () => 'template-react-ts'
+            message: chalk.blue(`${chalk.bold.red(`Build director`)}:`),
+            validate: function(val = '') {
+                return val !== '';
+            }
         }
-    ]).then(({ targetDir }) => {
-        
-        if (fs.existsSync(path.resolve(targetDir))) {
-            console.log(chalk.bold.red('The directory already exists, please input!'));
+    ]);
+
+    if (fs.existsSync(path.resolve(targetDir))) {
+        console.log(chalk.bold.red('The directory already exists, please input!'));
+        process.exit(0);
+    }
+
+    spinner.start("generated...");
+
+    exec('git clone https://github.com/56496477/typescript-cli.git', (error) => {
+
+        if (error) {
+            console.log(chalk.bold.red('Pull error !'));
             process.exit(0);
         }
 
-        spinner.start("generated...");
+        if (targetDir !== 'typescript-cli') {
+            exec(`mv typescript-cli/ ${targetDir}`);
+        }
 
-        exec('git clone https://github.com/56496477/typescript-cli.git', (error, stdout, stderr) => {
+        exec(`rm -rf ${targetDir}/.git`);
 
-            if (error) {
-                console.log(chalk.bold.red('Pull error !'));
-                process.exit(0);
-                return;
-            }
+        spinner.succeed('Successful...!');
 
-            if (targetDir !== 'template-react-ts') {
-                exec(`mv typescript-cli/ ${targetDir}`);
-            }
+        console.log(chalk.green`The project has been built successfully: `)
+        console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
+        console.log(chalk.blue(`2.yarn or npm install`));
+        console.log(chalk.blue(`enjoy it !`));
 
-            exec(`rm -rf ${targetDir}/.git`);
-
-            spinner.succeed('Successful...!');
-
-            console.log(chalk.green`The project has been built successfully: `)
-            console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
-            console.log(chalk.blue(`2.yarn or npm install`));
-            console.log(chalk.blue(`enjoy it !`));
-
-        });
-    })
+    });
 }
 
 
