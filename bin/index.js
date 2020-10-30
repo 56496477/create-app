3467ffc4c4d3597c961adc87a51e817df075f9ad
niufang <56496477@qq.com>
2020-10-30 17:10:51 +0800

    add..

--- bin/index.js
+++ bin/index.js
@@ -12,11 +12,15 @@ const spinner = new Ora();
 
 program.version("0.0.9", "-v, --version", "new version message");
 
+const gits = {
+    js: 'https://github.com/56496477/template-react.git',
+    ts: 'https://github.com/56496477/typescript-cli.git'
+}
+
 program
   .command("create")
   .description("Create a new react template")
   .action(async () => {
-
     const { type } = await inquirer.prompt([
         {
             type: 'list',
@@ -28,18 +32,15 @@ program
             ]
         }
     ])
-
     if(type === 'javascript') {
         await downloadJs();
     }
     if(type === 'typescript') {
         await downloadTs();
     }
-
 });
 
 async function downloadJs() {
-
     const { targetDir } = await inquirer.prompt([
         {
             type: 'input',
@@ -50,42 +51,26 @@ async function downloadJs() {
             }
         }
     ]);
-
-    if(targetDir === 'template-react') {
-        console.log(chalk.bold.red('Please change directory!'));
-        process.exit(0);
-    }
-
     if (fs.existsSync(path.resolve(targetDir))) {
         console.log(chalk.bold.red('The directory already exists, please input!'));
         process.exit(0);
     }
-
     spinner.start("generated...");
-
-    exec('git clone https://github.com/56496477/template-react.git', (error) => {
-
+    exec(`git clone --depth=1 ${gits.js} ${targetDir}`, (error) => {
         if (error) {
             console.log(chalk.bold.red('Pull error !'));
             process.exit(0);
         }
-
-        exec(`mv template-react/ ${targetDir}`);
-
         exec(`rm -rf ${targetDir}/.git`);
-
         spinner.succeed('Successful...!');
-
         console.log(chalk.green`The project has been built successfully: `)
         console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
         console.log(chalk.blue(`2.yarn or npm install`));
         console.log(chalk.blue(`enjoy it !`));
-
     });
 }
 
 async function downloadTs() {
-
     const { targetDir } = await inquirer.prompt([
         {
             type: 'input',
@@ -96,39 +81,23 @@ async function downloadTs() {
             }
         }
     ]);
-
-    if(targetDir === 'typescript-cli') {
-        console.log(chalk.bold.red('Please change directory!'));
-        process.exit(0);
-    }
-
     if (fs.existsSync(path.resolve(targetDir))) {
         console.log(chalk.bold.red('The directory already exists, please input!'));
         process.exit(0);
     }
-
     spinner.start("generated...");
-
-    exec('git clone https://github.com/56496477/typescript-cli.git', (error) => {
-
+    exec(`git clone --depth=1 ${gits.ts} ${targetDir}`, (error) => {
         if (error) {
             console.log(chalk.bold.red('Pull error !'));
             process.exit(0);
         }
-
-        exec(`mv typescript-cli/ ${targetDir}`);
-
         exec(`rm -rf ${targetDir}/.git`);
-
         spinner.succeed('Successful...!');
-
         console.log(chalk.green`The project has been built successfully: `)
         console.log(chalk.blue(`1.Enter the working directory: cd ${targetDir}`));
         console.log(chalk.blue(`2.yarn or npm install`));
         console.log(chalk.blue(`enjoy it !`));
-
     });
 }
-
 
 program.parse(process.argv);
